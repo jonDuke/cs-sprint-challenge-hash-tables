96594ea6a7adb76923c3db4866f8ca579df41f92
jonDuke <jonduke90@gmail.com>
2020-08-07 08:56:35 -0700

    completed ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,9 +2,29 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # Add items from first list into the table
+    table = {}
+    for n in arrays[0]:
+        table[n] = False
 
-    return result
+    # For each other list...
+    for i in range(1, len(arrays)):
+        # Change each item found to True
+        for n in arrays[i]:
+            if n in table:
+                table[n] = True
+
+        # Delete any False items (were not found)
+        # Change everything back to False for next loop
+        keys = list(table.keys())  # copy keys to separate list so we can safely delete some
+        for key in keys:
+            if table[key]:
+                table[key] = False
+            else:
+                del(table[key])
+
+    # Remaining keys are the common elements
+    return list(table.keys())
 
 
 if __name__ == "__main__":
